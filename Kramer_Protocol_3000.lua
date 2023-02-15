792a2014796d3193681c75b0bb6ba27ce73b1905
Azwa32 <87596275+Azwa32@users.noreply.github.com>
2023-02-15 08:35:31 +0000

    update

--- Kramer_Protocol_3000.lua
+++ Kramer_Protocol_3000.lua
@@ -197,12 +197,9 @@ end
 
 
 function Select_Out_1_Input_2()
-  Controls['Output_1_Input_Button'][1].Boolean = false
-  Controls['Output_1_Input_Button'][2].Boolean = false
-  Controls['Output_1_Input_Button'][3].Boolean = false
-  Controls['Output_1_Input_Button'][4].Boolean = false
-  Controls['Output_1_Input_Button'][5].Boolean = false
-  Controls['Output_1_Input_Button'][6].Boolean = false
+  for input = 1, 6 do
+    Controls['Output_1_Input_Button'][input].Boolean = false
+  end
   Controls['Output_1_Input_Button'][2].Boolean = true
   Out_1_Input_Logic()
 end
